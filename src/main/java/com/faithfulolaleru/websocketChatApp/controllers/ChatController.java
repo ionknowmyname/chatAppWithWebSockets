1de75b7052d462f31bc8049a30d43a9024cb5b33
Faithful <olalerufaithful@gmail.com>
2022-09-21 13:30:59 +0100

    DONE

--- src/main/java/com/faithfulolaleru/websocketChatApp/controllers/ChatController.java
+++ src/main/java/com/faithfulolaleru/websocketChatApp/controllers/ChatController.java
@@ -5,7 +5,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ChatController {
 
     @MessageMapping("/chat.sendMessage")   // /app/chat.sendMessage
